d0a68f371e5c25ae13c04c0a880cf08bde3ffd69
lucaserib <lucasemanuelribeiro@hotmail.com>
2024-12-17 09:17:13 -0300

    finishing implementing views and some description elements

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import SearchForm from "../../components/SearchForm";
-import StartupCard from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
   searchParams,
@@ -10,9 +10,9 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const params = { search: query || null };
 
-  console.log(JSON.stringify(posts, null, 2));
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
@@ -35,7 +35,7 @@ export default async function Home({
 
         <ul className="mt-7 card_grid">
           {posts.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+            posts.map((post: StartupTypeCard, index: number) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
@@ -43,6 +43,7 @@ export default async function Home({
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,11 +1,11 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY =
-  defineQuery(`*[_type == "startup"&& defined(slug.current)] | order(_createdAt desc){
+  defineQuery(`*[_type == "startup"&& defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc){
   _id,
-  tittle,
+  title,
   slug, 
-  _createdAi,
+  _createdAt,
   author ->{
     _id, name,  image, bio},
   views,
@@ -13,3 +13,23 @@ export const STARTUPS_QUERY =
   category,
   image
 }`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0]{
+  _id,
+  title,
+  slug, 
+  _createdAt,
+  author ->{
+    _id, name, username, image, bio
+    },
+  views,
+  description,
+  category,
+  image,
+  pitch,
+}`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+  *[_type == "startup" && _id ==$id][0]{
+  _id, views }`);
